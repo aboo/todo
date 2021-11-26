41c58805f48cfb3e47bb3c5587329c0ffdea70c7
Zahra <zahra.hosseini8119@gmail.com>
2021-11-26 17:11:38 +0330

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,71 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:todo/base/routes.dart';
+import 'package:todo/screens/detail.dart';
+import 'package:todo/screens/home.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-void main() {
-  runApp(MyApp());
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  updateStatusBar();
+  Vx.setPathUrlStrategy();
+  try {
+    await Firebase.initializeApp();
+  } catch (Exception) {
+    print("not install firebase $Exception");
+  }
+  runApp(TodoApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: TodoApp(),
-    );
-  }
+void updateStatusBar() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.blue,
+      statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
 }
 
 class TodoApp extends StatelessWidget {
-  const TodoApp({Key key}) : super(key: key);
+   TodoApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: _buildApp(),
+    return MaterialApp.router(
+      // title: 'To Do',
+      onGenerateTitle: (BuildContext context) =>
+      AppLocalizations.of(context)!.title,
+      useInheritedMediaQuery: true,
+      routerDelegate: navigator,
+      routeInformationParser: VxInformationParser(),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      debugShowCheckedModeBanner: false,
+      backButtonDispatcher: RootBackButtonDispatcher(),
     );
   }
 
-  _buildApp() {
-    return Text("Implement Todo App here!");
-  }
+  final navigator = VxNavigator(
+    notFoundPage: (uri, params) => MaterialPage(
+      key: const ValueKey('not-found-page'),
+      child: Builder(
+        builder: (context) => Scaffold(
+          body: Center(
+            child: Text('Page ${uri.path} not found'),
+          ),
+        ),
+      ),
+    ),
+    routes: {
+      '/': (_, __) => MaterialPage(child: Home()),
+      Routes.newTask: (_, params) => MaterialPage(child: const Detail(), arguments: params),
+      Routes.detail: (_, params) =>  MaterialPage(child: const Detail(), arguments: params),
+    },
+  );
 }
